3413bb3e612457ebb91504099602690254e8208f
rachelsin <rmpodo@gmail.com>
2021-06-24 13:54:46 +0300

    start create user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Login from "./componnets/login/login";
 import store from "./redux/store/store";
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CreateUser from "./componnets/createUser/createUser";
+
 
 function App() {
   return (
     <Provider store={store}>
-     <Login/>
+      <Router>
+        <Switch>
+          <Route path="/" component={CreateUser} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
